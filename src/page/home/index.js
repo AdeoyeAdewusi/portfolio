fdc761847cb5e9f197da77692c65342a67c50694
Olayiwola <agbajeolaiwola@gmail.com>
2021-06-21 21:54:23 +0100

    first commit

---
+++ src/page/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../home/style.css";
+import { Top } from '../../components';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Top/>
+        </div>
+    )
+}
+
+export default Home
