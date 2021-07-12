3954750fcd65b95a59e6d0024e23560355366ed9
Olayiwola <agbajeolaiwola@gmail.com>
2021-07-12 01:10:24 +0100

    'portfolio_background'

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,11 @@
 
 import './style.css'
 import React from 'react'
-import { Models } from './3DModels';
 
 const Portfolio = () => {
     return (
     <div id="Portfolio" className='portfolio'>
     <div>
-      <Models/>
 
     </div>
     <div className='Portfolio_3dDisplay'>
