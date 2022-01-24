f00827ea30287083afbda1c66bffc97dd0435634
Olayiwola <agbajeolaiwola@gmail.com>
2022-01-24 08:01:31 +0100

    'portfolio'

--- src/api/contact/index.js
+++ src/api/contact/index.js
@@ -1,43 +1,30 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$rest_json = file_get_contents("php://input");
-$_GET = json_decode($rest_json, true);
 
-if (empty($_GET['name']) && empty($_GET['email'])) die();
-
-if ($_GET)
-	{
-
-	// set response code - 200 OK
-
-	http_response_code(200);
-	$subject = $_GET['name'];
-	$to = "agbajeolaiwola@gmail.com";
-	$from = $_GET['email'];
-
-	// data
-
-	$msg = $_GET['message'];
-
-	// Headers
-
-	$headers = "MIME-Version: 1.0\r\n";
-	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
-	$headers.= "From: <" . $from . ">";
-	mail($to, $subject, $msg, $headers);
-
-	// echo json_encode( $_POST );
-
-	echojson_encode(array(
-		"sent" => true
-	));
-	}
-  else
-	{
-
-	// tell the user about error
-
-	echojson_encode(["sent" => false, "message" => "Something went wrong"]);
-	}
-
-?>
+export default function (req, res) {
+    require('dotenv').config()
+    
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+        user: 'propsvest@gmail.com',
+        pass: process.env.password,
+      },
+      secure: true,
+    })
+    const mailData = {
+      from: req.body.email,
+      to: 'propsvest@gmail.com',
+      subject: `Message From ${req.body.subject}`,
+      text: req.body.message + " | Sent from: " + req.body.email,
+      html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`
+    }
+    transporter.sendMail(mailData, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info)
+    })
+    res.status(200)
+  }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import chart from './assets/Screenshot (420).png'
 import crm from './assets/Screenshot (221).png'
 import instagramClone from './assets/instagramclone.png'
-import aura from './assets/Screenshot (421).png'
+import aura from './assets/ebike.png'
 import ansel from './assets/ansel.png'
 import troniclab from './assets/troniclab.png'
 import scalehive from './assets/scalehive.png'
@@ -27,50 +27,18 @@ const Portfolio = () => {
                <Link to={{ pathname: 'https://github.com/agbajeolayiwola/reactInstagramClone_SocialHub' }} target={'_blank'}>GitHub</Link>
             </div>
             <div>
-                <img src={chart} alt='olasPortfolio' className='portfoioImg'/>  
-                <h4>A chart that monitors assets in a factory in real time</h4>  
-                          
-            </div>
-            <div>
                 <img src={crm} alt='olasPortfolio' className='portfoioImg'/>
                 <h4>Carrotsuite CRM Software</h4>
                 
             </div>
             <div>
                 <img src={aura} alt='olasPortfolio' className='portfoioImg'/>
-                <h4>ReactJs news website built for a client</h4>
+                <h4>eBike a landing page for a modern bike eCommerce Website</h4>
+                <Link to={{ pathname: 'https://lucid-mayer-485420.netlify.app/' }} target={'_blank'}>eBike</Link>
             </div>
             
         </div>
 
-        <div  className='portfolio_Grid'>
- 
-            <div>
-               <img src={instagramClone} alt='olasPortfolio' className='portfoioImg'/> 
-               <h4>Instagram clone built with ReactJs, and Firebase.</h4>
-               
-               <Link to={{ pathname: 'https://github.com/agbajeolayiwola/reactInstagramClone_SocialHub' }} target={'_blank'}>GitHub</Link>
-            </div>
-            <div>
-                <img src={ansel} alt='olasPortfolio' className='portfoioImg'/>  
-                <h4>Ansel page design adapted from figma board</h4>  
-                
-                <Link to={{ pathname:'https://ansel-admin-site.netlify.app/'}} target={'_blank'}>Hosted Page</Link>          
-            </div>
-            <div>
-                <img src={troniclab} alt='olasPortfolio' className='portfoioImg'/>
-                <h4>Full functioning troniclab website built with HTML/CSS, Javascript, and PHP for Contact</h4>
-                
-                <Link to={{ pathname:'http://troniclab.com/'}} target={'_blank'}>Troniclab Website</Link>
-            </div>
-            <div>
-                <img src={scalehive} alt='olasPortfolio' className='portfoioImg'/>
-                <h4>LinkedIn Type Social Media Built with HTML/CSS, Javascript, PHP, and MySql for handling database</h4>
-                
-                <Link to={{ pathname:'https://www.scalehive.co'}} target={'_blank'}>SCalehive Website</Link>
-            </div>
-            
-        </div>
     </div>
     )
 }
